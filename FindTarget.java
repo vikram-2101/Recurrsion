2a1d3eac9b71670e40db67dba9239e93055977af
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-11-29 09:36:19 +0530

    Update FindTarget.java

--- FindTarget.java
+++ FindTarget.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class FindTarget {
     public static void main(String[] args) {
-        / int[] arr = {1, 2, 3, 4, 5, 6};
+        // int[] arr = {1, 2, 3, 4, 5, 6};
         // System.out.println(find(arr, 5, 0));
         // }
         // static boolean find(int[] arr, int target, int index) {
