7a8cfb1a2668c8d62a44897cef8293aee1d89bda
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2023-12-28 20:10:30 +0530

    Update Subsequences.java

--- Subsequences.java
+++ Subsequences.java
@@ -1,4 +1,4 @@
-gdpublic class Subsequences {
+ggdpublic class Subsequences {
     public static void subsequences(String str, int idx, String newString) { 
         if (idx == str.length()) {
             System.out.println(newString);
