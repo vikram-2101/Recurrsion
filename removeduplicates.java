0953a191d0722528ea5e3447c8a81db82f043902
Vikram Kumar <145278357+vikram-2101@users.noreply.github.com>
2024-01-11 20:52:59 +0530

    Update removeduplicates.java

--- removeduplicates.java
+++ removeduplicates.java
@@ -1,4 +1,4 @@
-jfxddpublic class removeduplicates {
+public class removeduplicates {
     public static boolean[]  map = new boolean[26];
 
     public static void removeDuplicates(String str, int idx, String newString) {
